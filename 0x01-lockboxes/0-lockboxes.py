3d6e8bb18d4254a5159bfa57171a25b757201929
Laracious <omolaraamoke22@gmail.com>
2023-08-02 18:30:08 -0700

    pyton

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,20 @@
-s module contains a function that checks if boxes can be unlocked"""
+#!/usr/bin/python3
+"""
+0. Pascal's Triangle
+"""
 
 
-def canUnlockAll(boxes):
+def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
     """
-        checks if all boxes can be unlocked
-        boxes is a list of lists
-    """
-
-    # create a list to hold all the box numbers from box 1
-    # box numbers are the box indexes
-    # 0 ommited cos the first box (box at index 0) is assumed to be unlocked
-    box_numbers = list(range(1, len(boxes)))
-
-    # loop through box_numbers. Each number represents key to unlock the box
-    for key in box_numbers:
-        found_key = False  # Key not found initially
-        for index, box in enumerate(boxes):
-            # skip the box at index being the key value
-            # A box can only hold keys to other boxes and not itself
-            if index == key:
-                continue
-            if key in box:  # if the key is contained in a box, key is found
-                found_key = True
-                break   # No need continuing with loop as key is already found
-
-        # if internal loop finishes with key not found
-        if not found_key:
-            return False
-
-    # If loop finishes without returning false
-    # Means all keys was found
-    return True
+    res = []
+    if n > 0:
+        for i in range(1, n + 1):
+            level = []
+            C = 1
+            for j in range(1, i + 1):
+                level.append(C)
+                C = C * (i - j) // j
+            res.append(level)
+    return res
